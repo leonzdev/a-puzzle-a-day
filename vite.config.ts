c6a0bae488bc1b8e674dc20f672693aa9df6c1f3
leonzdev <leonz.dev@gmail.com>
2024-10-25 23:01:53 +0000

    Enable Chrome App

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,27 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'A Puzzle A Day',
+      short_name: 'Puzzle',
+      description: 'Play the game or solve it!',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   base: "https://leonzdev.github.io/a-puzzle-a-day/",
   appType: "mpa"
 });
